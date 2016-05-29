0cd2b85d9179e4dbe7a832e41a50529ba79c22a8
Lax-T <laxtec@gmail.com>
2016-05-30 00:53:52 +0300

    Update ln1_f4.py

--- ln1_f4.py
+++ ln1_f4.py
@@ -4,6 +4,7 @@
 import os
 import json
 import datetime
+import time
 import smtplib
 import subprocess
 from email.mime.multipart import MIMEMultipart
@@ -20,6 +21,7 @@ EXCEL_TABLE_NAME = '/home/lax/PycharmProjects/learn1/new method test.xlsx'
 SENDER_EADRESS = '***@gmail.com'
 SENDER_EPASSWORD = '***'
 RECEIVER_EADRESS = '***@gmail.com'
+DATETIME_TEMPLATE = '%Y-%d-%m %H:%M:%S'
 
 
 def get_sysresinfo(command):
@@ -56,122 +58,128 @@ def get_hdd_info():
 
 class SysinfoDatabase(object):
     """ Current database structure
-    {'2016,05,19,01,05,05': {
-                            'cpu_user': 45.33,
-                            'cpu_sys': 17.22,
-                            'cpu_total': 60.11,
-                            ....
-                            'hdd_free':5952
-                            }
+    {timestamp (in seconds):  {
+                               'cpu_user': 45.33,
+                               'cpu_sys': 17.22,
+                               'cpu_total': 60.11,
+                               ...
+                               'hdd_free':5952
+                               }
     } """
-    def __init__(self, db_file_name):
+    def __init__(self, db_file_name, datetime_format):
         self.db_file_name = db_file_name
+        self.datetime_format = datetime_format
         if not os.path.isfile(self.db_file_name):
             self.sysinfo_database = {}
             with open(self.db_file_name, 'w') as self.database_file:
                 self.database_file.write(json.dumps(self.sysinfo_database))
-            self.db_is_empty = True
-
         else:
-            self.db_is_empty = False
             with open(self.db_file_name, 'r') as self.database_file:
                 self.sysinfo_database = self.database_file.read().strip()
                 self.sysinfo_database = json.loads(self.sysinfo_database)
 
-        self.db_index_keywords = self.sysinfo_database.keys()
-        self.db_index_keywords.sort(reverse=True)
-
-        self.lastrh = None  # Variable definitions
-        self.periods_averaged = None
-        self.select_result = None
-        self.averaging_period_result = None
-        self.single_record_data = None
-        self.averaged_in_period = 0
-        self.current_period_timestamp = None
-        self.database_size = 0
-        self.new_record_data = {}
+        self.db_index_timestamps = self.sysinfo_database.keys()
+        self.db_index_timestamps.sort(reverse=True)
         self.sysinfo_keywords = ['cpu_user', 'cpu_sys', 'cpu_total', 'cpu_idle', 'mem_total', 'mem_used',
-                    'mem_free', 'mem_cached', 'hdd_total', 'hdd_used', 'hdd_free']
+                                 'mem_free', 'mem_cached', 'hdd_total', 'hdd_used', 'hdd_free']
 
     def get_last_record_hour(self):  # Returns hour of last record in database
-        self.lastrh = self.db_index_keywords[0]
-        return int(self.lastrh.split(',')[3])
+        return datetime.datetime.fromtimestamp(float(self.db_index_timestamps[0])).hour
 
-    def select(self, start=None, end=None, limit=12, groupbyhour=True):  # Select and average data from database
-        self.periods_averaged = 0
-        self.select_result = {}
-        if self.db_is_empty:
-            return self.select_result, self.periods_averaged
+    def select(self, start=None, end=None, hour_periods_limit=12):  # Select from database
+        select_result = {}
+        if not self.sysinfo_database:
+            return select_result
         if start is None:
-            start = self.db_index_keywords[0]
+            start = self.db_index_timestamps[0]
+        else:
+            start = str(time.mktime(datetime.datetime.strptime(start, self.datetime_format).timetuple()))
         if end is None:
-            end = self.db_index_keywords[len(self.db_index_keywords) - 1]
+            end = self.db_index_timestamps[len(self.db_index_timestamps) - 1]
+        else:
+            end = str(time.mktime(datetime.datetime.strptime(end, self.datetime_format).timetuple()))
+        if hour_periods_limit < 0:
+            hour_periods_limit = 12
 
-        self.averaging_period_result = {'cpu_user': 0, 'cpu_sys': 0, 'cpu_total': 0, 'cpu_idle': 0,
-                                        'mem_total': 0, 'mem_used': 0, 'mem_free': 0, 'mem_cached': 0,
-                                        'hdd_total': 0, 'hdd_used': 0, 'hdd_free': 0
-                                        }
-        self.current_period_timestamp = None
-        self.averaged_in_period = 0
-        self.single_record_data = None
+        current_period_timestamp = []
+        periods_in_select_result = 0
 
-        for current_key in self.db_index_keywords:
-            if start >= current_key >= end:
-                if self.current_period_timestamp is None:  # Setting up first avg period
-                    self.current_period_timestamp = current_key[0:13]
-
-                if groupbyhour:
-                    if self.current_period_timestamp != current_key[0:13]:
-                        for key in self.sysinfo_keywords:  # Avg and Add data to sel. result
-                            self.averaging_period_result[key] /= self.averaged_in_period
-                        self.select_result[self.current_period_timestamp] = self.averaging_period_result
-                        self.periods_averaged += 1
-                        self.averaging_period_result = {'cpu_user': 0, 'cpu_sys': 0, 'cpu_total': 0,
-                                                        'cpu_idle': 0, 'mem_total': 0, 'mem_used': 0,
-                                                        'mem_free': 0, 'mem_cached': 0, 'hdd_total': 0,
-                                                        'hdd_used': 0, 'hdd_free': 0
-                                                        }
-                        self.averaged_in_period = 0
-                        if self.periods_averaged >= limit:
-                            break
-                        self.current_period_timestamp = current_key[0:13]
-
-                self.single_record_data = self.sysinfo_database[current_key]  # Summing average
-                for key in self.sysinfo_keywords:
-                    self.averaging_period_result[key] += self.single_record_data[key]
-                self.averaged_in_period += 1
+        for index_timestamp in self.db_index_timestamps:
+            if start >= index_timestamp >= end:
+                index_timestamp_asobject = datetime.datetime.fromtimestamp(float(index_timestamp))
+                if current_period_timestamp != index_timestamp_asobject.strftime('%Y,%m,%d,%H'):
+                    current_period_timestamp = index_timestamp_asobject.strftime('%Y,%m,%d,%H')
+                    periods_in_select_result += 1
+                select_result[index_timestamp_asobject] = self.sysinfo_database[index_timestamp]
+            if periods_in_select_result >= hour_periods_limit:
+                break
             else:
-                if current_key < end:
+                if index_timestamp < end:
                     break
-        if self.averaged_in_period != 0:
-            for key in self.sysinfo_keywords:  # Avg and Add data to sel. result
-                self.averaging_period_result[key] /= self.averaged_in_period
-            self.select_result[self.current_period_timestamp] = self.averaging_period_result
-            self.periods_averaged += 1
-        return self.select_result, self.periods_averaged
+        return select_result
+
+    def average(self, input_data, groupbyhour=True):
+        input_data_timestamps = input_data.keys()
+        input_data_timestamps.sort(reverse=True)
+
+        avg_period_result = {'cpu_user': 0, 'cpu_sys': 0, 'cpu_total': 0, 'cpu_idle': 0,
+                             'mem_total': 0, 'mem_used': 0, 'mem_free': 0, 'mem_cached': 0,
+                             'hdd_total': 0, 'hdd_used': 0, 'hdd_free': 0
+                             }
+        average_result = {}
+        periods_in_result = 0
+        averaged_in_period = 0
+        period_timestamp = None
+
+        for in_data_timestamp in input_data_timestamps:
+            if not period_timestamp:
+                period_timestamp = in_data_timestamp.strftime('%Y,%m,%d,%H')
+
+            if groupbyhour:
+                if period_timestamp != in_data_timestamp.strftime('%Y,%m,%d,%H'):
+                    for key in self.sysinfo_keywords:
+                        avg_period_result[key] /= averaged_in_period
+                    average_result[datetime.datetime.strptime(period_timestamp, '%Y,%m,%d,%H')] = avg_period_result
+                    periods_in_result += 1
+                    averaged_in_period = 0
+                    avg_period_result = {'cpu_user': 0, 'cpu_sys': 0, 'cpu_total': 0, 'cpu_idle': 0,
+                                         'mem_total': 0, 'mem_used': 0, 'mem_free': 0, 'mem_cached': 0,
+                                         'hdd_total': 0, 'hdd_used': 0, 'hdd_free': 0
+                                         }
+                    period_timestamp = in_data_timestamp.strftime('%Y,%m,%d,%H')
+            single_db_record = input_data[in_data_timestamp]
+            for key in self.sysinfo_keywords:
+                avg_period_result[key] += single_db_record[key]
+            averaged_in_period += 1
+
+        if averaged_in_period != 0:
+            for key in self.sysinfo_keywords:
+                avg_period_result[key] /= averaged_in_period
+                average_result[datetime.datetime.strptime(period_timestamp, '%Y,%m,%d,%H')] = avg_period_result
+            periods_in_result += 1
+        return average_result, periods_in_result
 
     def new_record(self, timestamp, data):  # Adding new record into database
-        self.sysinfo_database[timestamp] = data
+        self.sysinfo_database[time.mktime(timestamp.timetuple())] = data
         with open(self.db_file_name, 'w') as self.database_file:
             self.database_file.write(json.dumps(self.sysinfo_database))
-        self.db_index_keywords = self.sysinfo_database.keys()
-        self.db_index_keywords.sort(reverse=True)
+        self.db_index_timestamps = self.sysinfo_database.keys()
+        self.db_index_timestamps.sort(reverse=True)
 
     def erase(self):  # Database full erase
         self.sysinfo_database = {}
-        self.db_is_empty = True
         with open(self.db_file_name, 'w') as self.database_file:
             self.database_file.write(json.dumps(self.sysinfo_database))
 
     def clean(self, size_limit=500):  # Cleans database from old records (default is 500 record limit)
-        self.database_size = len(self.db_index_keywords)
-        while self.database_size > size_limit:
-            del self.sysinfo_database[self.db_index_keywords[self.database_size - 1]]
-            self.database_size -= 1
+        database_size = len(self.db_index_timestamps)
+        while database_size > size_limit:
+            del self.sysinfo_database[self.db_index_timestamps[database_size - 1]]
+            database_size -= 1
         with open(self.db_file_name, 'w') as self.database_file:
             self.database_file.write(json.dumps(self.sysinfo_database))
-        self.db_index_keywords = self.sysinfo_database.keys()
-        self.db_index_keywords.sort(reverse=True)
+        self.db_index_timestamps = self.sysinfo_database.keys()
+        self.db_index_timestamps.sort(reverse=True)
 
 ###################################################################################################################
 
@@ -189,7 +197,7 @@ def start_html_table():
 def extend_html_table(e_html_table, data, timestamp):
     e_html_table += """
                 <tr>
-                    <td>Averaging period {13}.{12}.{11} {14}:00 - {14}:59</td>
+                    <td>Averaging period {11} {12}:00 - {12}:59</td>
                 </tr>
                 <tr>
                     <td>CPU</td>
@@ -214,8 +222,8 @@ def extend_html_table(e_html_table, data, timestamp):
     """.format(data['cpu_total'], data['cpu_user'], data['cpu_sys'],
                data['cpu_idle'], data['mem_total'], data['mem_used'],
                data['mem_free'], data['mem_cached'], data['hdd_total'],
-               data['hdd_used'], data['hdd_free'], timestamp[2],
-               timestamp[1], timestamp[0], timestamp[3])
+               data['hdd_used'], data['hdd_free'], timestamp.date(),
+               timestamp.hour)
 
     return e_html_table
 
@@ -288,8 +296,8 @@ class ExcelTable(object):
         self.row4_data = []
 
     def table_data_update(self, data, timestamp):
-        self.row1_data = ['Averaging period %s.%s.%s %s:00 - %s:59' % (timestamp[2], timestamp[1], timestamp[0],
-                                                                       timestamp[3], timestamp[3]), '', '', '', '']
+        self.row1_data = ['Averaging period %s %s:00 - %s:59' % (timestamp.date(), timestamp.hour, timestamp.hour),
+                          '', '', '', '']
         self.row2_data = ['CPU', 'total: %.2f' % (data['cpu_total']), 'user: %.2f' % (data['cpu_user']), 'system: %.2f'
                           % (data['cpu_sys']), 'idle: %.2f' % (data['cpu_idle'])]
         self.row3_data = ['Memory', 'total: %d' % (data['mem_total']), 'used: %d'
@@ -377,32 +385,31 @@ def update_additional_database(adb_file_name, a_database):
 
 if __name__ == '__main__':
     system_usage_info = dict(get_cpu_info().items() + get_mem_info().items() + get_hdd_info().items())
-    print system_usage_info
-    systime_customformat = datetime.datetime.now()
-    systime_customformat = systime_customformat.strftime('%Y,%m,%d,%H,%M,%S')
+    system_time = datetime.datetime.now()
 
-    systeminfo_database = SysinfoDatabase(DATABASE_FILE_NAME)
-    select_result, periods_in_sel_result = systeminfo_database.select()
+    systeminfo_database = SysinfoDatabase(DATABASE_FILE_NAME, DATETIME_TEMPLATE)
+    select_result = systeminfo_database.select()
+    averaging_result, periods_in_avg_result = systeminfo_database.average(select_result)
 
     additional_database = load_additionad_database(ADD_DATABASE_FILE_NAME)
     last_em_send_hour = additional_database['last_em_send_hour']
     emails_sent = additional_database['emails_sent']
-    current_system_hour = int(systime_customformat.split(',')[3])
+    current_system_hour = system_time.hour
 
     html_table = start_html_table()
 
-    if last_em_send_hour != current_system_hour+1:  # check if averaging period changed and need to send email
-        sel_res_timestamps = select_result.keys()
-        sel_res_timestamps.sort(reverse=True)
-        for index, timestamp in enumerate(sel_res_timestamps):  # 5 - table size limit
-            html_table = extend_html_table(html_table, select_result[timestamp], timestamp.split(','))
+    if last_em_send_hour != current_system_hour+1 and periods_in_avg_result >= 1:  # check if averaging period changed and need to send email
+        avg_res_timestamps = averaging_result.keys()
+        avg_res_timestamps.sort(reverse=True)
+        for index, timestamp_key in enumerate(avg_res_timestamps):  # 5 - table size limit
+            html_table = extend_html_table(html_table, averaging_result[timestamp_key], timestamp_key)
             if index >= 4:
                 break
         html_table = end_html_table(html_table)
         if emails_sent >= 11:  # 11 - is to include excel table in every 12th email (every 12 hours)
             new_excel_table = ExcelTable()
-            for index, timestamp in enumerate(sel_res_timestamps):  # 12 - table size limit
-                new_excel_table.extend(select_result[timestamp], timestamp.split(','))
+            for index, timestamp_key in enumerate(avg_res_timestamps):  # 12 - table size limit
+                new_excel_table.extend(averaging_result[timestamp_key], timestamp_key)
                 if index >= 11:
                     break
             new_excel_table.save(EXCEL_TABLE_NAME)
@@ -416,5 +423,5 @@ if __name__ == '__main__':
         additional_database['last_em_send_hour'] = current_system_hour
         update_additional_database(ADD_DATABASE_FILE_NAME, additional_database)
 
-    systeminfo_database.new_record(systime_customformat, system_usage_info)
+    systeminfo_database.new_record(system_time, system_usage_info)
     systeminfo_database.clean()
